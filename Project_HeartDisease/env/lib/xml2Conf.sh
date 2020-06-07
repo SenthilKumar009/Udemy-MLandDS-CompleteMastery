838b9e0befd9c1cf2ac28c008e022dc612ff336b
skkthenotorious <senthilkumark.nitt@gmail.com>
2020-06-07 11:41:42 +0530

    Adding Project heart disease

---
+++ Project_HeartDisease/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project_HeartDisease/env/lib"
+XML2_LIBS="-lxml2 -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project_HeartDisease/env/lib -lz -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project_HeartDisease/env/lib -llzma -lpthread -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project_HeartDisease/env/lib  -lm "
+XML2_INCLUDEDIR="-I/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project_HeartDisease/env/include/libxml2 -I/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project_HeartDisease/env/include"
+MODULE_VERSION="xml2-2.9.9"
+
