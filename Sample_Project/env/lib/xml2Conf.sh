6682f799f8621a6af6675c2971dae9104822ef89
skkthenotorious <senthilkumark.nitt@gmail.com>
2020-06-07 11:49:34 +0530

    Sample Project added

---
+++ Sample_Project/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Sample_Project/env/lib"
+XML2_LIBS="-lxml2 -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Sample_Project/env/lib -lz -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Sample_Project/env/lib -llzma -lpthread -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Sample_Project/env/lib  -lm "
+XML2_INCLUDEDIR="-I/home/skk_thenotorious/Documents/Udemy-MachineLearning/Sample_Project/env/include/libxml2 -I/home/skk_thenotorious/Documents/Udemy-MachineLearning/Sample_Project/env/include"
+MODULE_VERSION="xml2-2.9.9"
+
