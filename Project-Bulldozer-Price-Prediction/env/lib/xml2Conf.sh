6bfe0ed64cfe0a729ba4a6774e2b544e870a7282
skkthenotorious <senthilkumark.nitt@gmail.com>
2020-06-14 22:29:54 +0530

    Bulldozers Price prediction project is added

---
+++ Project-Bulldozer-Price-Prediction/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project-Bulldozer-Price-Prediction/env/lib"
+XML2_LIBS="-lxml2 -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project-Bulldozer-Price-Prediction/env/lib -lz -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project-Bulldozer-Price-Prediction/env/lib -llzma -lpthread -L/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project-Bulldozer-Price-Prediction/env/lib  -lm "
+XML2_INCLUDEDIR="-I/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project-Bulldozer-Price-Prediction/env/include/libxml2 -I/home/skk_thenotorious/Documents/Udemy-MachineLearning/Project-Bulldozer-Price-Prediction/env/include"
+MODULE_VERSION="xml2-2.9.9"
+
